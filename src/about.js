5a1fa6609695af40fdb98091d6b7a2147d0c69dc
work <137403706+AltheaVestrit@users.noreply.github.com>
2025-07-22 16:44:36 +0200

    add address content to about tab with js

--- src/about.js
+++ src/about.js
@@ -9,5 +9,25 @@ export default (function() {
     const aboutInfo = content.appendChild(document.createElement("div"));
     aboutInfo.id = "about-info-container";
 
+    // Address
+    const addressContainer = aboutInfo.appendChild(document.createElement("div"));
+    addressContainer.classList.add("info-item");
+
+    const addressHeader = addressContainer.appendChild(document.createElement("h2"));
+    addressHeader.classList.add("info-item-header");
+    addressHeader.innerText = "Address";
+
+    const addressContent = addressContainer.appendChild(document.createElement("div"));
+    addressContent.classList.add("info-item-content");
+
+    const addressLine1 = addressContent.appendChild(document.createElement("p"));
+    addressLine1.innerText = "Saffron & Stone";
+
+    const addressLine2 = addressContent.appendChild(document.createElement("p"));
+    addressLine2.innerText = "Kerkstraat 42";
+
+    const addressLine3 = addressContent.appendChild(document.createElement("p"));
+    addressLine3.innerText = "5504 HR Veldhoven";
+
     return content;
 })();
